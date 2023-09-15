32359015e8a1879a6591b240fffa8d514996cbde
Lahtinen <luuklah@gmail.com>
2023-09-15 18:11:55 +0300

    kortti ulkonäkö ja playercard objekti

--- 2023Syksy_CardGame/src/App.jsx
+++ 2023Syksy_CardGame/src/App.jsx
@@ -1,9 +1,16 @@
 import Card from './components/Card'
+import './App.css'
+
+
+const playerCard = {
+  image: "http://placekitten.com/120/100",
+  stats:[{name: 'Cuteness', value: 8}],
+};
 
 export default function App(){
   return(
    <div>
-     <h1>Hello wordl!</h1>
+     <h1>Hello world!</h1>
      <Card />
    </div>
    );
--- 2023Syksy_CardGame/src/components/Card.jsx
+++ 2023Syksy_CardGame/src/components/Card.jsx
@@ -1,7 +1,14 @@
 export default function App(){
-    return(
-     <div>
-       <img src="http://placekitten.com/120/100"/>
-     </div>
-     );
-  };
+ return(
+   <div className="card">
+      <img src="http://placekitten.com/120/100"/>
+      <ul className="stat-list">
+        <li className="stat-list-item">
+          <span>Cuteness</span>
+          <span>10</span>
+       </li>
+     </ul>
+        
+   </div>
+  );
+ }
