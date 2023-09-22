b20cc84dd793a97543924aa162cfc1e2ec572423
Lahtinen <luuklah@gmail.com>
2023-09-22 18:12:31 +0300

    map ja enemycard

--- 2023Syksy_CardGame/src/App.jsx
+++ 2023Syksy_CardGame/src/App.jsx
@@ -4,14 +4,24 @@ import './App.css'
 
 const playerCard = {
   image: "http://placekitten.com/120/100",
-  stats:[{name: 'Cuteness', value: 8}],
+  stats:[
+    {name: 'Cuteness', value: 8},
+    {name: 'Speed', value: 7}]
+};
+
+const enemyCard = {
+  image: "http://placekitten.com/120/100?image=14",
+  stats:[
+    {name: 'Cuteness', value: 10},
+    {name: 'Speed', value: 5}]
 };
 
 export default function App(){
   return(
    <div>
      <h1>Hello world!</h1>
-     <Card />
+     <Card card = {playerCard}/>
+     <Card card = {enemyCard}/>
    </div>
    );
 };
