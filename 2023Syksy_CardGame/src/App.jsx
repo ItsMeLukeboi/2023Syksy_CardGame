0276466520ac74ab72ddfe7c3ed106263e697600
Lahtinen <luuklah@gmail.com>
2023-09-29 18:13:35 +0300

    voi pelata jotenki ja ulkonäkö

--- 2023Syksy_CardGame/src/App.jsx
+++ 2023Syksy_CardGame/src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 const playerCard = {
   image: "http://placekitten.com/120/100",
   stats:[
-    {name: 'Cuteness', value: 8},
+    {name: 'Cuteness', value: 10},
     {name: 'Speed', value: 7}]
 };
 
@@ -17,11 +17,37 @@ const enemyCard = {
 };
 
 export default function App(){
+
+  function compareCards(){
+    const playerStat = playerCard.stats[0];
+    const enemyStat = enemyCard.stats[0];
+
+    let result = '';
+
+    if(playerStat.value === enemyStat.value){
+      result = 'draw'
+    }
+
+    else if(playerStat.value > enemyStat.value){
+      result = 'victory'
+    }
+
+    else{
+      result = 'defeat'
+    }
+
+
+    console.log(result);
+  }
+
   return(
    <div>
      <h1>Hello world!</h1>
-     <Card card = {playerCard}/>
-     <Card card = {enemyCard}/>
+     <div className='game'>
+      <Card card = {playerCard}/>
+      <button onClick={compareCards} type="button">Play</button>
+      <Card card = {enemyCard}/>
+     </div>
    </div>
    );
 };
