87a37dae0968f20208ac85c38670bcdabdab0a0e
Lahtinen <luuklah@gmail.com>
2023-10-13 18:13:36 +0300

    pakka ja pelaajan kortit näkyvii

--- 2023Syksy_CardGame/src/App.jsx
+++ 2023Syksy_CardGame/src/App.jsx
@@ -1,53 +1,85 @@
 import Card from './components/Card'
 import './App.css'
+import { useState } from 'react';
+
+const getRandoInt = (min,max) => Math.floor(Math.random() * (max - min + 1) + min);
 
 
 const playerCard = {
-  image: "http://placekitten.com/120/100",
+  image: "http://placekitten.com/120/100?image="+getRandoInt(0,15),
   stats:[
-    {name: 'Cuteness', value: 10},
-    {name: 'Speed', value: 7}]
+    {name: 'Cuteness', value: getRandoInt(0,100)},
+    {name: 'Speed', value: getRandoInt(0,100)}]
 };
 
 const enemyCard = {
-  image: "http://placekitten.com/120/100?image=14",
+  image: "http://placekitten.com/120/100?image="+getRandoInt(0,15),
   stats:[
-    {name: 'Cuteness', value: 10},
-    {name: 'Speed', value: 5}]
+    {name: 'Cuteness', value: getRandoInt(0,100)},
+    {name: 'Speed', value: getRandoInt(0,100)}]
 };
 
+const createCard = index =>({
+  image: "http://placekitten.com/120/100?image="+ index,
+  stats:[
+    {name: 'Cuteness', value: getRandoInt(0,100)},
+    {name: 'Chonk', value: getRandoInt(0,100)},
+    {name: 'Speed', value: getRandoInt(0,100)}],
+  id:crypto.randomUUID()
+})
+
+const deck = Array(16).fill(null).map((_,index) => createCard(index));
+const half = Math.ceil(deck.length / 2);
+const dealCards = () =>{
+  return{
+    player: deck.slice(0,half),
+    enemy: deck.slice(half)
+  };
+}
+
+
 export default function App(){
-
+  const[result, setResult] = useState('');
+  const[cards, setCards] = useState(dealCards);
   function compareCards(){
-    const playerStat = playerCard.stats[0];
-    const enemyStat = enemyCard.stats[0];
+    const playerStat = cards.player[0].stats[0];
+    const enemyStat = cards.enemy[0].stats[0];
 
-    let result = '';
 
     if(playerStat.value === enemyStat.value){
-      result = 'draw'
+      setResult('Draw')
     }
 
     else if(playerStat.value > enemyStat.value){
-      result = 'victory'
+      setResult('Win')
     }
 
     else{
-      result = 'defeat'
+      setResult('Loss')
     }
 
 
-    console.log(result);
   }
 
   return(
    <div>
      <h1>Hello world!</h1>
      <div className='game'>
-      <Card card = {playerCard}/>
-      <button onClick={compareCards} type="button">Play</button>
-      <Card card = {enemyCard}/>
+      <ul className='card-list'>
+        {cards.player.map(pCard =>(
+          <li className='card-list-item player' key={pCard.id}>
+            <Card card = {pCard}/>
+          </li>
+        ))}
+      </ul>
+      
+      <div className='center'>
+        <p>{result || 'Press the button'}</p>
+       <button onClick={compareCards} type="button">Play</button>
+      </div>
+      <Card card = {cards.enemy[0]}/>
      </div>
+     {console.log(dealCards())}
    </div>
    );
 };
