5d333ea669eff1fc1b58f7d7f82e9461652361d1
Lahtinen <luuklah@gmail.com>
2023-12-08 18:14:29 +0200

    breakout aloitus, canvas +pallo

--- 2023Syksy_CardGame/src/App.jsx
+++ 2023Syksy_CardGame/src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Home from './components/Koti'
 import CardGame from './components/cardgame/Cardgame'
+import Breakout from './components/breakout/breakout'
 
 export default function App(){
   return(
@@ -16,12 +17,16 @@ export default function App(){
             <li>
               <Link to="/cardgame">CardGame</Link>
             </li>
+            <li>
+              <Link to="/breakout">Breakout</Link>
+            </li>
           </ul>
         </nav>
       </div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cardgame" element={<CardGame/>}/>
+        <Route path="/breakout" element={<Breakout/>}/>
       </Routes>
     </Router>
   );
